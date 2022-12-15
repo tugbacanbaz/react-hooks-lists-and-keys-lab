f38c7dbd2014fe10b5b7ae8a5cb9fb7dc63cdb6d
tugbacanbaz <tugbacanbaz.pi@gmail.com>
2022-12-15 19:32:45 +0300

    Complited Assignment

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -2,12 +2,19 @@ import React from "react";
 
 function ProjectItem({ name, about, technologies }) {
   return (
+    <div>
     <div className="project-item">
       <h3>{name}</h3>
-      <p>{about}</p>
-      <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
+      <h4>{about}</h4>
       </div>
+      <span className="technologies">
+        { technologies.map( item => {
+    return (
+      <span key={item}>{item}</span>
+    )
+  })}
+      </span>
+    
     </div>
   );
 }
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -6,7 +6,15 @@ function ProjectList({ projects }) {
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">{ projects.map( (item) => {
+    return (
+      <div key={item.id}>
+          <h1>{item.name}</h1>
+          <p>{item.about}</p>
+          <span className="technologies">{item.technologies.join(', ')}</span>
+      </div>
+    )
+  })}</div>
     </div>
   );
 }
